1467ead1d49372238ba09faad15a14a4b873edd9
minhngoc211193 <141900701+minhngoc211193@users.noreply.github.com>
2024-07-02 11:46:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,4 @@
-from flask import Flask, render_template, request
-import os, webbrowser, threading, json
-from utils import version
-from utils.process import get_results_pdf
-from time import time
-from datetime import datetime
-
-app = Flask(__name__)
-app.static_folder = 'static'
-
-LOG_FILE_PATH = 'static/process.txt'
-def log_submission(pdf_path, results):
-    with open(LOG_FILE_PATH, 'a') as file:
-        record = f"{datetime.now()}, {pdf_path}, {json.dumps(results)}\n"
-        file.write(record)
-
-
-@app.template_filter('format_number')
-def format_number(value):
-    """Format a number with commas as thousands separators."""
-    return f"{value:,}"
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/process', methods=['POST'])
-def process():
-    if request.method == 'POST':
-        pdf = request.files['pdf']
-        requested_date = request.form['text']
-        pdf_path = "temp/input_file.pdf"
-        pdf.save(pdf_path)
-
-        print("received requested date: ", requested_date)
-        output_temp_file = os.path.join("static", "output_img.png")
-        if os.path.isfile(output_temp_file):
-            os.remove(output_temp_file)
-
-        ocr_result = get_results_pdf(pdf_path, requested_date, inplace_img=True)
-        json_results = {
-            "insurance_balance": ocr_result["insurance"],
-            "insurance_event_date": ocr_result["event_date"],
-            "image_quality": ocr_result["image_quality"],
-            "detail_inf": ocr_result["detail_inf"],
-            "version": version,
-        }
-        insurance_event_date = datetime.strptime(json_results['insurance_event_date'], '%Y%m%d').strftime('%d/%m/%Y')
-        log_submission(pdf_path, json_results)
-        return render_template("result.html", time=time, result=json_results, insurance_event_date=insurance_event_date)
-
-@app.route("/history")
-def view_history():
-    with open(LOG_FILE_PATH, 'r') as file:
-        logs = file.readlines()
-    return '<br>'.join(logs)
-
-def open_browser():
-    webbrowser.open_new('http://127.0.0.1:5000/')
-
-if __name__ == '__main__':
-    threading.Timer(1, open_browser).start()
-    app.run(debug=False)
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import os, webbrowser, threading, json
 from utils import version
 from utils.process import get_results_pdf
@@ -151,7 +87,22 @@ def rate():
     file_id = request.form.get('file_id')
     accuracy = request.form.get('accuracy')
     update_accuracy_log(file_id, accuracy)
-    return "Thank you for your feedback!"
+
+    # Get updated results
+    with open(LOG_FILE_PATH, 'r') as file:
+        logs = file.readlines()
+    result = None
+    for log in logs:
+        if file_id in log:
+            result = log.strip().split(", ")
+            break
+
+    if result:
+        json_results = json.loads(result[7])
+        insurance_event_date = datetime.strptime(json_results['insurance_event_date'], '%Y%m%d').strftime('%d/%m/%Y')
+        return render_template("result.html", time=time, result=json_results, insurance_event_date=insurance_event_date, feedback="Thank you for your feedback!")
+    else:
+        return "Error: File ID not found"
 
 def open_browser():
     webbrowser.open_new('http://127.0.0.1:5000/')
@@ -159,6 +110,3 @@ def open_browser():
 if __name__ == '__main__':
     threading.Timer(1, open_browser).start()
     app.run(debug=True)
-
-
-
