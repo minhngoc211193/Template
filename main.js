56abd40b95b58cd5ff977cdcd7f3f57c735448e0
minhngoc211193 <141900701+minhngoc211193@users.noreply.github.com>
2024-07-30 13:53:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,34 @@
       });
 
 })(jQuery);
+// src/App.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    // Gửi yêu cầu HTTP GET đến API của Strapi để lấy danh sách bài viết
+    axios.get('http://localhost:1337/articles')
+      .then(response => {
+        setArticles(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the articles!', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Articles</h1>
+      <ul>
+        {articles.map(article => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
